82904406cd4ccb948e496f4184020cd6da6aa995
Lee Stratman <stratmanlc10@gmail.com>
2021-04-17 12:22:20 -0500

    Update league model.

--- models/league.js
+++ models/league.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 const Layout = require("../models/layout");
-const Event = require("../models/event");
 
 const leagueSchema = new mongoose.Schema({
   public: { type: Boolean, default: true },
-  name: { type: String, required: true, minlength: 3 },
-  organizer: { type: mongoose.Types.ObjectId, ref: "User" },
+  name: { type: String, required: true, minlength: 3, unique: true },
+  organizer: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   users: [{ type: mongoose.Types.ObjectId, ref: "User" }],
-  regDeadline: { type: Date },
   layouts: [Layout.schema],
+  regDeadline: { type: Date },
   createdDate: { type: Date, default: Date.now() },
-  events: [Event.schema],
+  city: { type: String },
+  state: { type: String },
+  zip: { type: String },
+  description: { type: String },
+  startDate: { type: Date },
+  endDate: { type: Date },
 });
 
 const League = mongoose.model("League", leagueSchema);
