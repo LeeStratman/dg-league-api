98e8285e41aa5a353c7d2396bad80f2fcf52ce36
Lee Stratman <stratmanlc10@gmail.com>
2021-04-17 18:34:44 -0500

    Simplify layout schema.

--- models/layout.js
+++ models/layout.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
-const Hole = require("../models/hole");
 
 const layoutSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  distance: { type: Number },
-  source: { type: String },
   courseId: { type: String, required: true },
-  holes: [Hole.schema],
-  public: { type: Boolean, default: false },
+  tee_pos: { Type: Number },
 });
 
 const Layout = mongoose.model("Layout", layoutSchema);
