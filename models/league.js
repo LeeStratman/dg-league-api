996c6dc0dca57d1e0f3d42ff9dd55cc21cff07e6
Lee Stratman <stratmanlc10@gmail.com>
2021-04-17 18:36:23 -0500

    Add courses to league schema.

--- models/league.js
+++ models/league.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const Layout = require("../models/layout");
 
 const leagueSchema = new mongoose.Schema({
   public: { type: Boolean, default: true },
   name: { type: String, required: true, minlength: 3, unique: true },
   organizer: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   users: [{ type: mongoose.Types.ObjectId, ref: "User" }],
-  layouts: [Layout.schema],
+  courses: [{ type: String }],
   regDeadline: { type: Date },
   createdDate: { type: Date, default: Date.now() },
   city: { type: String },
