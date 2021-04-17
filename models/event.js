c3ada8b912d3e977f1a0d9633cd3442983593137
Lee Stratman <stratmanlc10@gmail.com>
2021-04-17 18:37:32 -0500

    Update the event model

--- models/event.js
+++ models/event.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+const Layout = require("../models/layout");
 const Scorecard = require("../models/scorecard");
 const Result = require("../models/result");
 
 const eventSchema = new mongoose.Schema({
-  name: { type: String },
+  name: { type: String, required: true },
   description: { type: String },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
-  layout: { type: mongoose.Types.ObjectId, required: true, ref: "Layout" },
+  layout: Layout.schema,
   published: { type: Boolean, default: false },
   status: {
     type: String,
@@ -17,7 +18,7 @@ const eventSchema = new mongoose.Schema({
   results: [Result.schema],
   calculatedDate: { type: Date },
   leagueId: { type: mongoose.Types.ObjectId, required: true, ref: "League" },
-  scorecards: [Scorecard.schema], // Denormalize on purpose, so user can't modify scorecard.  They can delete from scorecard collection, but it remains here.
+  scorecards: [Scorecard.schema], // Denormalize on purpose, so user can't modify scorecard. They can delete from scorecard collection, but it remains here.
 });
 
 const Event = mongoose.model("Event", eventSchema);
