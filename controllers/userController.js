3cca947b2c8f57121778fa2fad742c3c6f131b6e
Lee Stratman <stratmanlc10@gmail.com>
2021-04-18 13:07:59 -0500

    Update user controller functions.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,36 @@
 const User = require("../models/user");
-const { getUserLeagues, getLeagueWithUsers } = require("../utils/league");
 const crudController = require("../utils/crud");
 const { ResourceExistsError, ServerError } = require("../utils/error");
-const { userExists } = require("../utils/user");
 
-const getLeagues = async (req, res, next) => {
-  const { id } = req.params;
+// const getLeagues = async (req, res, next) => {
+//   const { id } = req.params;
 
-  try {
-    const leagues = await getUserLeagues(id);
+//   try {
+//     // const leagues = await getUserLeagues(id);
 
-    return res.status(200).send(leagues);
-  } catch (err) {
-    next(new ServerError(err));
-  }
-};
+//     return res.status(200).send();
+//   } catch (err) {
+//     next(new ServerError(err));
+//   }
+// };
 
-const getLeague = async (req, res, next) => {
-  const { id, leagueId } = req.params;
+// const getLeague = async (req, res, next) => {
+//   const { id, leagueId } = req.params;
 
-  try {
-    const validUser = await userExists(id);
+//   try {
+//     const validUser = await userExists(id);
 
-    if (!validUser) return next(new ResourceExistsError("User"));
+//     if (!validUser) return next(new ResourceExistsError("User"));
 
-    const league = await getLeagueWithUsers(leagueId);
+//     // const league = await getLeagueWithUsers(leagueId);
 
-    if (!league) return next(new ResourceExistsError("League"));
+//     // if (!league) return next(new ResourceExistsError("League"));
 
-    return res.status(200).send(league);
-  } catch (err) {
-    next(new ServerError(err));
-  }
-};
+//     // return res.status(200).send(league);
+//   } catch (err) {
+//     next(new ServerError(err));
+//   }
+// };
 
 const removeOne = async (req, res, next) => {
   const { id } = req.params;
@@ -53,7 +51,5 @@ const removeOne = async (req, res, next) => {
 
 module.exports = {
   ...crudController(User),
-  getLeagues,
-  getLeague,
   removeOne,
 };
