e865e857af64b191b56d6d0b12951b59c19937df
Lee Stratman <stratmanlc10@gmail.com>
2021-04-18 14:22:09 -0500

    Remove commented code from user controller file.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,36 +2,6 @@ const User = require("../models/user");
 const crudController = require("../utils/crud");
 const { ResourceExistsError, ServerError } = require("../utils/error");
 
-// const getLeagues = async (req, res, next) => {
-//   const { id } = req.params;
-
-//   try {
-//     // const leagues = await getUserLeagues(id);
-
-//     return res.status(200).send();
-//   } catch (err) {
-//     next(new ServerError(err));
-//   }
-// };
-
-// const getLeague = async (req, res, next) => {
-//   const { id, leagueId } = req.params;
-
-//   try {
-//     const validUser = await userExists(id);
-
-//     if (!validUser) return next(new ResourceExistsError("User"));
-
-//     // const league = await getLeagueWithUsers(leagueId);
-
-//     // if (!league) return next(new ResourceExistsError("League"));
-
-//     // return res.status(200).send(league);
-//   } catch (err) {
-//     next(new ServerError(err));
-//   }
-// };
-
 const removeOne = async (req, res, next) => {
   const { id } = req.params;
 
