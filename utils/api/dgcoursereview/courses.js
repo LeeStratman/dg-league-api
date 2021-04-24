b629e3a6d9eeb58e9e5ee8dbf9f5b42c3ae11838
Lee Stratman <stratmanlc10@gmail.com>
2021-04-24 12:33:11 -0500

    Add getCoursesByIds function.

--- utils/api/dgcoursereview/courses.js
+++ utils/api/dgcoursereview/courses.js
@@ -15,7 +15,44 @@ const getCourse = async (id) => {
   );
 };
 
+const getCoursesByIds = async (courseIds) => {
+  try {
+    const promises = courseIds.map(async (courseId) => {
+      const course = await getCourse(courseId).then((course) => course.data);
+
+      const courseInfo = course[0];
+      const holes = course.filter((hole) => hole.hole_num);
+      const { tee_1_clr, tee_2_clr, tee_3_clr, tee_4_clr } = courseInfo;
+      const tees = [];
+
+      if (tee_1_clr) tees.push(1);
+      if (tee_2_clr) tees.push(2);
+      if (tee_3_clr) tees.push(3);
+      if (tee_4_clr) tees.push(4);
+      const layouts = [];
+
+      for (let i = 0; i < tees.length; i++) {
+        const layout = holes.map((hole) => {
+          return {
+            length: hole[`tee_${tees[i]}_len`],
+            par: hole[`tee_${tees[i]}_par`],
+          };
+        });
+        layouts.push(layout);
+      }
+      return { ...courseInfo, layouts };
+    });
+
+    const courses = await Promise.all(promises);
+
+    return courses;
+  } catch (err) {
+    return [];
+  }
+};
+
 module.exports = {
   getCourse,
   searchCourses,
+  getCoursesByIds,
 };
